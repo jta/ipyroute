e8be8d77c0644ee3b6a4cdb591dcfb0215024df9
João Taveira Araújo <joao.taveira@gmail.com>
2016-05-10 13:02:45 +0100

    Add support for deprecated tag.

--- ipyroute/address.py
+++ ipyroute/address.py
@@ -10,6 +10,7 @@ class Address(base.Base):
                        r'(brd (?P<brd>\S+))?\s?'
                        r'(peer (?P<peer>\S+))?\s?'
                        r'scope (?P<scope>\S+) '
+                       r'(?P<deprecated>deprecated)?\s?'
                        r'(mngtmpaddr (?P<mngtmpaddr>\S+))?\s?'
                        r'(?P=ifname)?((:(?P<label>[^\\]+)))?')
 
